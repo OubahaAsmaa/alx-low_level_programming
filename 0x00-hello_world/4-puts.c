c01003e8a7271f5f7a71ef658e70a4295bde2fbe
OubahaAsmaa <oubahaasmaa1337@gmail.com>
2023-09-07 15:10:53 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-        puts("\"Pogramming is like building a multilingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
