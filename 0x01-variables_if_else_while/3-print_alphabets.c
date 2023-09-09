249783ac408b16aa2639458e3ff40dd71d628f10
OubahaAsmaa <oubahaasmaa1337@gmail.com>
2023-09-08 18:08:22 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *lowercase and uppercase using putchar and ascii table
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n = 97;
+	int m = 65;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m <= 90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
+}
