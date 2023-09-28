55cf3665ef0791e6a22e03356e57a635564a6ab2
OubahaAsmaa <oubahaasmaa1337@gmail.com>
2023-09-28 16:25:38 -0700

    #

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _print_rev_recursion - Prints revese string.
+ * @s: The string to be printed.
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+}
